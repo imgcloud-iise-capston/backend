384ce4a3e67a5d8f64b56e54297c1cc138852bba
soeun1230 <ybbsoen1230@naver.com>
2024-05-12 17:26:29 +0900

    predict model 위한 dependencies 설정

--- src/main/java/iise_capston/imgcloud/controller/BrisqueController.java
+++ src/main/java/iise_capston/imgcloud/controller/BrisqueController.java
@@ -1,2 +1,33 @@
-package iise_capston.imgcloud.controller;public class BrisqueController {
+package iise_capston.imgcloud.controller;
+
+import iise_capston.imgcloud.service.SvmTrainService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@RestController
+public class BrisqueController {
+
+    private final SvmTrainService svmTrainService;
+    Logger logger = LoggerFactory.getLogger(BrisqueController.class);
+
+    @PostMapping("/make/train/svm")
+    ResponseEntity<String> makeTrainSvm(
+            @RequestPart("trainData") MultipartFile trainData
+            ){
+
+        svmTrainService.saveSvmModel(trainData);
+
+
+        return ResponseEntity.ok("savedNewModel");
+    }
+
+
+
 }
---
+++ src/main/java/iise_capston/imgcloud/service/BrisqueService.java
@@ -0,0 +1,12 @@
+package iise_capston.imgcloud.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class BrisqueService {
+
+
+}
