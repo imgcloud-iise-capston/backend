f956fb9589f6f0cdd5b351074036e0f77b613ba7
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-05-14 19:11:56 +0900

    Update BrisqueController.java

--- src/main/java/iise_capston/imgcloud/controller/BrisqueController.java
+++ src/main/java/iise_capston/imgcloud/controller/BrisqueController.java
@@ -1,7 +1,9 @@
 package iise_capston.imgcloud.controller;
 
+import iise_capston.imgcloud.service.BrisqueService;
 import iise_capston.imgcloud.service.SvmTrainService;
 import lombok.RequiredArgsConstructor;
+import org.opencv.core.Core;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RequiredArgsConstructor
 @RestController
 public class BrisqueController {
 
     private final SvmTrainService svmTrainService;
+    private final BrisqueService brisqueService;
     Logger logger = LoggerFactory.getLogger(BrisqueController.class);
 
     @PostMapping("/make/train/svm")
@@ -24,10 +29,24 @@ public class BrisqueController {
 
         svmTrainService.saveSvmModel(trainData);
 
-
         return ResponseEntity.ok("savedNewModel");
     }
 
+    @PostMapping("/calculate/brisque")
+    ResponseEntity<String> calBrisque(
+            @RequestPart("image") MultipartFile image
+    ){
+        double score = 0;
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        try{
+            logger.info("run");
+            score = brisqueService.test_measure_BRISQUE(image);
+            logger.info("run end");
+        }catch (IOException e){}
+
+        logger.info("score : "+score);
+        return ResponseEntity.ok("ok");
+    }
 
 
 }
