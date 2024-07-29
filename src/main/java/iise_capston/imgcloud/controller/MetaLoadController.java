7aa8aa50db05951a07a731304ca8af48009599f7
soeun1230 <ybbsoen1230@naver.com>
2024-07-29 22:42:41 +0900

    meta load fix

--- src/main/java/iise_capston/imgcloud/controller/MetaLoadController.java
+++ src/main/java/iise_capston/imgcloud/controller/MetaLoadController.java
@@ -2,7 +2,9 @@ package iise_capston.imgcloud.controller;
 
 import iise_capston.imgcloud.domain.dto.PeopleMetadataDto;
 import iise_capston.imgcloud.domain.dto.ThingMetadataDto;
+import iise_capston.imgcloud.domain.repository.PeopleImageMemberRepository;
 import iise_capston.imgcloud.domain.repository.PeopleMetadataMemberRepository;
+import iise_capston.imgcloud.domain.repository.ThingImageMemberRepository;
 import iise_capston.imgcloud.domain.repository.ThingMetadataMemberRepository;
 import iise_capston.imgcloud.member.MetadataMember;
 import iise_capston.imgcloud.member.PeopleImageMember;
@@ -21,16 +23,20 @@ public class MetaLoadController {
 
     private ThingMetadataMemberRepository thingMetadataMemberRepository;
     private PeopleMetadataMemberRepository peopleMetadataMemberRepository;
+    private ThingImageMemberRepository thingImageMemberRepository;
+    private PeopleImageMemberRepository peopleImageMemberRepository;
 
     @GetMapping("load/meta/thing")
-    public MetadataMember getThingMeta(@RequestParam ThingImageMember thingId){
-        MetadataMember thingMetadata = thingMetadataMemberRepository.findBythingId(thingId).get();
+    public MetadataMember getThingMeta(@RequestParam Long thingId){
+        ThingImageMember thingImageMember = thingImageMemberRepository.findById(thingId).get();
+        MetadataMember thingMetadata = thingMetadataMemberRepository.findBythingId(thingImageMember).get();
         return thingMetadata;
     }
 
     @GetMapping("load/meta/people")
-    public MetadataMember getPeopleMeta(@RequestParam PeopleImageMember peopleId){
-        MetadataMember peopleMetadata = peopleMetadataMemberRepository.findBypeopleId(peopleId).get();
+    public MetadataMember getPeopleMeta(@RequestParam Long peopleId){
+        PeopleImageMember peopleImageMember = peopleImageMemberRepository.findById(peopleId).get();
+        MetadataMember peopleMetadata = peopleMetadataMemberRepository.findBypeopleId(peopleImageMember).get();
         return peopleMetadata;
     }
 }
