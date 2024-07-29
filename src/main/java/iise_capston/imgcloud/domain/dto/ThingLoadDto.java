6456878314d547cfc3a235976d04636e910c7360
soeun1230 <ybbsoen1230@naver.com>
2024-07-29 23:47:13 +0900

    load fix

--- src/main/java/iise_capston/imgcloud/domain/dto/ThingLoadDto.java
+++ src/main/java/iise_capston/imgcloud/domain/dto/ThingLoadDto.java
@@ -1,2 +1,24 @@
-package iise_capston.imgcloud.domain.dto;public class ThingLoadDto {
+package iise_capston.imgcloud.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ThingLoadDto {
+    private Integer ISO;
+    private Double FStop;
+    private Integer ExposureTime;
+    private String GPSLatitude;
+    private String GPSLongitude;
+    private String RealResolution;
+    private String Resolution;
+    private String WhiteBalance;
+    private String size;
+    private Integer metaScore;
+
 }
