d9d5e4bea6ae8d0d151c86e22d81efda4c4882c6
soeun1230 <ybbsoen1230@naver.com>
2024-08-10 12:25:57 +0900

    matlab bridque function add

--- src/main/java/iise_capston/imgcloud/controller/BrisqueController.java
+++ src/main/java/iise_capston/imgcloud/controller/BrisqueController.java
@@ -12,17 +12,26 @@ import iise_capston.imgcloud.service.SmallFileService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.bytedeco.opencv.opencv_core.Scalar;
-import org.objectweb.asm.TypeReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -96,20 +105,53 @@ public class BrisqueController {
         InithingImageMember.setUserThingId(uploadedUser);
         List<CompletableFuture<String>> url =  new ArrayList<>();
 
-        try {
-            completablescores = brisqueService.getBrisqueAll(image);
 
-            for (CompletableFuture<Scalar> cnow : completablescores) {
-                Scalar now = cnow.get();
-                double score = Math.round(now.get(0));
-                finalScores.add((100 - (int) score));
+        String matlabUrl = "https://<matlab-server-address>/analyze"; // MATLAB 서버의 엔드포인트 URL
 
+        for(int i=0;i<image.size();i++){
+            MultipartFile img = image.get(i);
+            // HttpClient 생성
+            try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+                HttpPost uploadFile = new HttpPost(matlabUrl);
+
+                // MultipartEntityBuilder를 사용하여 이미지 파일을 전송
+                MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+                builder.addBinaryBody("file", img.getInputStream(),
+                        ContentType.MULTIPART_FORM_DATA, img.getOriginalFilename());
+
+                HttpEntity multipart = builder.build();
+                uploadFile.setEntity(multipart);
+
+                // MATLAB 서버로 요청 전송
+                try (CloseableHttpResponse response = httpClient.execute(uploadFile)) {
+                    HttpEntity responseEntity = response.getEntity();
+
+                    String result = new String(responseEntity.getContent().readAllBytes(), StandardCharsets.UTF_8);
+
+                    finalScores.add(Integer.parseInt(result));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                logger.error("Error calculating BRISQUE scores", e);
+                return ResponseEntity.status(500).build();
             }
-
-        } catch (Exception e) {
-            logger.error("Error calculating BRISQUE scores", e);
-            return ResponseEntity.status(500).build();
         }
+
+        //원해 코드
+//        try {
+//            completablescores = brisqueService.getBrisqueAll(image);
+//
+//            for (CompletableFuture<Scalar> cnow : completablescores) {
+//                Scalar now = cnow.get();
+//                double score = Math.round(now.get(0));
+//                finalScores.add((100 - (int) score));
+//
+//            }
+//
+//        } catch (Exception e) {
+//            logger.error("Error calculating BRISQUE scores", e);
+//            return ResponseEntity.status(500).build();
+//        }
 
 
 
@@ -179,6 +221,42 @@ public class BrisqueController {
         double width = 0;
         double height = 0;
 
+//        try {
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            Map<String, Object> cropData = objectMapper.readValue(cropDataJson, Map.class);
+//
+//            x = convertToDouble(cropData.get("x"));
+//            y = convertToDouble(cropData.get("y"));
+//            width = convertToDouble(cropData.get("width"));
+//            height = convertToDouble(cropData.get("height"));
+//            BufferedImage originalImage = ImageIO.read(new ByteArrayInputStream(image.getBytes()));
+//            width = Math.min(width, originalImage.getWidth() - x);
+//            height = Math.min(height, originalImage.getHeight() - y);
+//            BufferedImage croppedImage = originalImage.getSubimage((int) x, (int) y, (int) width, (int) height);
+//
+//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//            ImageIO.write(croppedImage, fileType, baos);
+//            byte[] croppedBytes = baos.toByteArray();
+//
+//            MultipartFile croppedMultipartFile = new MockMultipartFile("croppedImage", "croppedImage." + fileType, "image/" + fileType, croppedBytes);
+//            List<MultipartFile> croppedImages = new ArrayList<>();
+//            croppedImages.add(croppedMultipartFile);
+//
+//            List<CompletableFuture<Scalar>> completablescores = brisqueService.getBrisqueAll(croppedImages);
+//            for (CompletableFuture<Scalar> cnow : completablescores) {
+//                Scalar now = cnow.get();
+//                double score = Math.round(now.get(0));
+//                finalScores.add((100 - (int) score));
+//            }
+//        } catch (IOException e) {
+//            logger.error("Error reading image or parsing cropData", e);
+//            return ResponseEntity.status(500).body(null);
+//        } catch (Exception e) {
+//            logger.info("Error calculating BRISQUE scores " + e);
+//            return ResponseEntity.status(500).body(null);
+//        }
+
+
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             Map<String, Object> cropData = objectMapper.readValue(cropDataJson, Map.class);
@@ -187,31 +265,44 @@ public class BrisqueController {
             y = convertToDouble(cropData.get("y"));
             width = convertToDouble(cropData.get("width"));
             height = convertToDouble(cropData.get("height"));
+
+            // 원본 이미지 읽기
             BufferedImage originalImage = ImageIO.read(new ByteArrayInputStream(image.getBytes()));
+
+            // 크롭 영역이 이미지 범위를 넘지 않도록 조정
             width = Math.min(width, originalImage.getWidth() - x);
             height = Math.min(height, originalImage.getHeight() - y);
+
+            // 이미지 크롭
             BufferedImage croppedImage = originalImage.getSubimage((int) x, (int) y, (int) width, (int) height);
 
+            // 크롭된 이미지를 ByteArray로 변환
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(croppedImage, fileType, baos);
-            byte[] croppedBytes = baos.toByteArray();
+            ImageIO.write(croppedImage, "jpg", baos);
+            byte[] croppedImageBytes = baos.toByteArray();
 
-            MultipartFile croppedMultipartFile = new MockMultipartFile("croppedImage", "croppedImage." + fileType, "image/" + fileType, croppedBytes);
-            List<MultipartFile> croppedImages = new ArrayList<>();
-            croppedImages.add(croppedMultipartFile);
+            String matlabUrl = "https://<matlab-server-address>/analyze"; // MATLAB 서버의 엔드포인트 URL
 
-            List<CompletableFuture<Scalar>> completablescores = brisqueService.getBrisqueAll(croppedImages);
-            for (CompletableFuture<Scalar> cnow : completablescores) {
-                Scalar now = cnow.get();
-                double score = Math.round(now.get(0));
-                finalScores.add((100 - (int) score));
+            try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+                HttpPost uploadFile = new HttpPost(matlabUrl);
+
+                MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+                builder.addBinaryBody("file", new ByteArrayInputStream(croppedImageBytes),
+                        ContentType.MULTIPART_FORM_DATA, image.getOriginalFilename());
+
+                HttpEntity multipart = builder.build();
+                uploadFile.setEntity(multipart);
+
+                try (CloseableHttpResponse response = httpClient.execute(uploadFile)) {
+                    HttpEntity responseEntity = response.getEntity();
+
+                    String result = new String(responseEntity.getContent().readAllBytes(), StandardCharsets.UTF_8);
+                    finalScores.add(Integer.parseInt(result));
+                }
             }
         } catch (IOException e) {
-            logger.error("Error reading image or parsing cropData", e);
-            return ResponseEntity.status(500).body(null);
-        } catch (Exception e) {
-            logger.info("Error calculating BRISQUE scores " + e);
-            return ResponseEntity.status(500).body(null);
+            e.printStackTrace();
+            return ResponseEntity.status(500).build();
         }
 
         PeopleImageMember peopleImageMemberSave = new PeopleImageMember();
--- src/main/java/iise_capston/imgcloud/controller/ContinuousImageController.java
+++ src/main/java/iise_capston/imgcloud/controller/ContinuousImageController.java
@@ -1,22 +1,25 @@
 package iise_capston.imgcloud.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import iise_capston.imgcloud.domain.dto.ThingImageUploadDto;
-import iise_capston.imgcloud.domain.dto.ThingMetadataDto;
-import iise_capston.imgcloud.domain.dto.TitleDto;
 import iise_capston.imgcloud.domain.repository.PeopleImageMemberRepository;
 import iise_capston.imgcloud.member.OauthMember;
-import iise_capston.imgcloud.member.ThingImageMember;
+
 import iise_capston.imgcloud.service.BrisqueService;
 import iise_capston.imgcloud.service.MetadataService;
 import iise_capston.imgcloud.service.OauthService;
 import iise_capston.imgcloud.service.SmallFileService;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.bytedeco.opencv.opencv_core.Scalar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -28,10 +31,22 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import static net.minidev.asm.DefaultConverter.convertToDouble;
 
@@ -62,20 +77,52 @@ public class ContinuousImageController {
             return ResponseEntity.badRequest().build();
         }
 
-        try {
-            completablescores = brisqueService.getBrisqueAll(image);
+        String matlabUrl = "https://<matlab-server-address>/analyze";
 
-            for (CompletableFuture<Scalar> cnow : completablescores) {
-                Scalar now = cnow.get();
-                double score = Math.round(now.get(0));
-                finalScores.add((100 - (int) score));
+        for(int i=0;i<image.size();i++){
+            MultipartFile img = image.get(i);
+            // HttpClient 생성
+            try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+                HttpPost uploadFile = new HttpPost(matlabUrl);
 
+                // MultipartEntityBuilder를 사용하여 이미지 파일을 전송
+                MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+                builder.addBinaryBody("file", img.getInputStream(),
+                        ContentType.MULTIPART_FORM_DATA, img.getOriginalFilename());
+
+                HttpEntity multipart = builder.build();
+                uploadFile.setEntity(multipart);
+
+                // MATLAB 서버로 요청 전송
+                try (CloseableHttpResponse response = httpClient.execute(uploadFile)) {
+                    HttpEntity responseEntity = response.getEntity();
+
+                    String result = new String(responseEntity.getContent().readAllBytes(), StandardCharsets.UTF_8);
+
+                    finalScores.add(Integer.parseInt(result));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                logger.error("Error calculating BRISQUE scores", e);
+                return ResponseEntity.status(500).build();
             }
-
-        } catch (Exception e) {
-            logger.error("Error calculating BRISQUE scores", e);
-            return ResponseEntity.status(500).build();
         }
+
+
+//        try {
+//            completablescores = brisqueService.getBrisqueAll(image);
+//
+//            for (CompletableFuture<Scalar> cnow : completablescores) {
+//                Scalar now = cnow.get();
+//                double score = Math.round(now.get(0));
+//                finalScores.add((100 - (int) score));
+//
+//            }
+//
+//        } catch (Exception e) {
+//            logger.error("Error calculating BRISQUE scores", e);
+//            return ResponseEntity.status(500).build();
+//        }
 
 
         return ResponseEntity.ok(finalScores);
@@ -94,9 +141,49 @@ public class ContinuousImageController {
             return ResponseEntity.badRequest().build();
         }
 
+//        for(int i=0;i<images.size();i++){
+//            double x = 0;
+//            double y = 0;
+//            double width = 0;
+//            double height = 0;
+//            MultipartFile image = images.get(i);
+//            try {
+//                ObjectMapper objectMapper = new ObjectMapper();
+//                Map<String, Object> cropData = objectMapper.readValue(cropDataJson, Map.class);
+//
+//                x = convertToDouble(cropData.get("x"));
+//                y = convertToDouble(cropData.get("y"));
+//                width = convertToDouble(cropData.get("width"));
+//                height = convertToDouble(cropData.get("height"));
+//                BufferedImage originalImage = ImageIO.read(new ByteArrayInputStream(image.getBytes()));
+//                width = Math.min(width, originalImage.getWidth() - x);
+//                height = Math.min(height, originalImage.getHeight() - y);
+//                BufferedImage croppedImage = originalImage.getSubimage((int) x, (int) y, (int) width, (int) height);
+//
+//                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//                ImageIO.write(croppedImage, fileType, baos);
+//                byte[] croppedBytes = baos.toByteArray();
+//
+//                MultipartFile croppedMultipartFile = new MockMultipartFile("croppedImage", "croppedImage." + fileType, "image/" + fileType, croppedBytes);
+//                List<MultipartFile> croppedImages = new ArrayList<>();
+//                croppedImages.add(croppedMultipartFile);
+//
+//                List<CompletableFuture<Scalar>> completablescores = brisqueService.getBrisqueAll(croppedImages);
+//                for (CompletableFuture<Scalar> cnow : completablescores) {
+//                    Scalar now = cnow.get();
+//                    double score = Math.round(now.get(0));
+//                    finalScores.add((100 - (int) score));
+//                }
+//            } catch (IOException e) {
+//                logger.error("Error reading image or parsing cropData", e);
+//                return ResponseEntity.status(500).body(null);
+//            } catch (Exception e) {
+//                logger.info("Error calculating BRISQUE scores " + e);
+//                return ResponseEntity.status(500).body(null);
+//            }
+//        }
 
-
-        for(int i=0;i<images.size();i++){
+        for(int i=0;i<images.size();i++) {
             double x = 0;
             double y = 0;
             double width = 0;
@@ -110,31 +197,42 @@ public class ContinuousImageController {
                 y = convertToDouble(cropData.get("y"));
                 width = convertToDouble(cropData.get("width"));
                 height = convertToDouble(cropData.get("height"));
+
                 BufferedImage originalImage = ImageIO.read(new ByteArrayInputStream(image.getBytes()));
+
                 width = Math.min(width, originalImage.getWidth() - x);
                 height = Math.min(height, originalImage.getHeight() - y);
+
+                // 이미지 크롭
                 BufferedImage croppedImage = originalImage.getSubimage((int) x, (int) y, (int) width, (int) height);
 
+                // 크롭된 이미지를 ByteArray로 변환
                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                ImageIO.write(croppedImage, fileType, baos);
-                byte[] croppedBytes = baos.toByteArray();
+                ImageIO.write(croppedImage, "jpg", baos);
+                byte[] croppedImageBytes = baos.toByteArray();
 
-                MultipartFile croppedMultipartFile = new MockMultipartFile("croppedImage", "croppedImage." + fileType, "image/" + fileType, croppedBytes);
-                List<MultipartFile> croppedImages = new ArrayList<>();
-                croppedImages.add(croppedMultipartFile);
+                String matlabUrl = "https://<matlab-server-address>/analyze"; // MATLAB 서버의 엔드포인트 URL
 
-                List<CompletableFuture<Scalar>> completablescores = brisqueService.getBrisqueAll(croppedImages);
-                for (CompletableFuture<Scalar> cnow : completablescores) {
-                    Scalar now = cnow.get();
-                    double score = Math.round(now.get(0));
-                    finalScores.add((100 - (int) score));
+                try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+                    HttpPost uploadFile = new HttpPost(matlabUrl);
+
+                    MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+                    builder.addBinaryBody("file", new ByteArrayInputStream(croppedImageBytes),
+                            ContentType.MULTIPART_FORM_DATA, image.getOriginalFilename());
+
+                    HttpEntity multipart = builder.build();
+                    uploadFile.setEntity(multipart);
+
+                    try (CloseableHttpResponse response = httpClient.execute(uploadFile)) {
+                        HttpEntity responseEntity = response.getEntity();
+
+                        String result = new String(responseEntity.getContent().readAllBytes(), StandardCharsets.UTF_8);
+                        finalScores.add(Integer.parseInt(result));
+                    }
                 }
             } catch (IOException e) {
-                logger.error("Error reading image or parsing cropData", e);
-                return ResponseEntity.status(500).body(null);
-            } catch (Exception e) {
-                logger.info("Error calculating BRISQUE scores " + e);
-                return ResponseEntity.status(500).body(null);
+                e.printStackTrace();
+                return ResponseEntity.status(500).build();
             }
         }
 
